78fed288f3e0f0c35bd68d62361f25ed65527742
Jughead1996 <ksidibe@h-tsoft.com>
2022-08-13 20:36:59 +0000

    save

--- accueil.php
+++ accueil.php
@@ -154,7 +154,7 @@
       <div class="sidebar">
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-          <div class="info">
+          <div class="info" style=" display: block;margin-left: auto;margin-right: auto">
             <a href="#" class="d-block">Menu</a>
           </div>
         </div>
