72fed911fd1f5431f05a0e7ec7f6a4a6974903c5
SaliouGithub <serignesalioudione@esp.sn>
2022-08-16 00:40:03 +0000

    new

--- dashboard.php
+++ dashboard.php
@@ -1,3 +1,4 @@
+
   <!-- Include header et menu début -->
   <?php
     include('header.php');
@@ -10,15 +11,15 @@
     
       <!-- Main Début  -->
       <div class="content-header">
-      <div class="container-fluid">
-      <div class="row mb-2">
-          <div class="col-sm-6">
-          <h1 class="m-0">Tableau de bord</h1>
-          </div>
-          
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                  <h1 class="m-0">Tableau de bord</h1>
+                </div>
+                
+            </div>
+        </div>
       </div>
-      </div>
-  </div>
 
   <!-- Main content -->
       <section class="content">
@@ -94,8 +95,8 @@
 
    <!-- Include footer début -->
    <?php
-    include('footer.php');
-  ?>
+      include('footer.php');
+   ?>
   <!-- Include footer fin -->
 
   
@@ -130,6 +131,7 @@
 
 
 
+ 
 
 
 
@@ -201,44 +203,3 @@
 
 
 
-
-
-
-<!-- ./wrapper -->
-
-<!-- jQuery -->
-<script src="plugins/jquery/jquery.min.js"></script>
-<!-- jQuery UI 1.11.4 -->
-<script src="plugins/jquery-ui/jquery-ui.min.js"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
-  $.widget.bridge('uibutton', $.ui.button)
-</script>
-<!-- Bootstrap 4 -->
-<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- ChartJS -->
-<script src="plugins/chart.js/Chart.min.js"></script>
-<!-- Sparkline -->
-<script src="plugins/sparklines/sparkline.js"></script>
-<!-- JQVMap -->
-<script src="plugins/jqvmap/jquery.vmap.min.js"></script>
-<script src="plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
-<!-- jQuery Knob Chart -->
-<script src="plugins/jquery-knob/jquery.knob.min.js"></script>
-<!-- daterangepicker -->
-<script src="plugins/moment/moment.min.js"></script>
-<script src="plugins/daterangepicker/daterangepicker.js"></script>
-<!-- Tempusdominus Bootstrap 4 -->
-<script src="plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
-<!-- Summernote -->
-<script src="plugins/summernote/summernote-bs4.min.js"></script>
-<!-- overlayScrollbars -->
-<script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
-<!-- AdminLTE App -->
-<!-- <script src="dist/js/adminlte.js"></script> -->
-<!-- AdminLTE for demo purposes -->
-<script src="dist/js/demo.js"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="dist/js/pages/dashboard.js"></script>
-</body>
-</html>
--- footer.php
+++ footer.php
@@ -1,14 +1,14 @@
+
 <footer class="main-footer">
     <strong>Copyright &copy; 2022<a href="https://adminlte.io">HelpDesk</a>.</strong>
     All rights reserved.
   
-  </footer>
+</footer>
 
   <!-- Control Sidebar -->
   <aside class="control-sidebar control-sidebar-dark">
     <!-- Control sidebar content goes here -->
   </aside>
-  <!-- /.control-sidebar -->
 
 
   
