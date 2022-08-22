f644da853914326f5093b608d7d0de46bcbcea47
SaliouGithub <serignesalioudione@esp.sn>
2022-08-22 09:53:46 +0000

    new

--- index.php
+++ index.php
@@ -22,11 +22,11 @@
       <a href="index2.html" class="h1"><b>Help</b>Desk</a>
     </div>
     <div class="card-body">
-      <p class="login-box-msg">Sign in to start your session</p>
+      <p class="login-box-msg">Connexion</p>
 
       <form action="index3.html" method="post">
         <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Email">
+          <input type="text" class="form-control" placeholder="Login">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-envelope"></span>
@@ -34,7 +34,7 @@
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password">
+          <input type="password" class="form-control" placeholder="Mot de passe">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
@@ -42,37 +42,27 @@
           </div>
         </div>
         <div class="row">
-          <div class="col-8">
+          <!-- <div class="col-6">
             <div class="icheck-primary">
               <input type="checkbox" id="remember">
               <label for="remember">
-                Remember Me
+                Se souvenir de moi
               </label>
             </div>
-          </div>
+          </div> -->
           <!-- /.col -->
-          <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+          <div class="col-12">
+            <button type="submit" class="btn btn-primary btn-block">Se connecter</button>
           </div>
           <!-- /.col -->
         </div>
       </form>
-<!-- 
-      <div class="social-auth-links text-center mt-2 mb-3">
-        <a href="#" class="btn btn-block btn-primary">
-          <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-        </a>
-        <a href="#" class="btn btn-block btn-danger">
-          <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-        </a>
-      </div> -->
-      <!-- /.social-auth-links -->
 
       <p class="mb-1">
-        <a href="forgot-password.html">I forgot my password</a>
+        <a href="forgot-password.html">J'ai oublié mon mot de passe</a>
       </p>
       <p class="mb-0">
-        <a href="inscription.php" class="text-center">Register a new membership</a>
+        <a href="inscription.php" class="text-center">Créer un compte</a>
       </p>
     </div>
     <!-- /.card-body -->
--- php/controller/logged.php
+++ php/controller/logged.php
@@ -16,41 +16,26 @@
 			$_SESSION['helpdeskemail'] = $infos[0]['email'];
 			$_SESSION['helpdeskmotDePasse'] = $infos[0]['motDePasse'];
 			$_SESSION['helpdesklogin'] = $infos[0]['login'];
-			$_SESSION['helpdesknomComplet'] = $infos[0]['nomComplet'];
+			$_SESSION['helpdesknom'] = $infos[0]['nom'];
+			$_SESSION['helpdeskprenom'] = $infos[0]['prenom'];
 			$_SESSION['helpdesktelephone'] = $infos[0]['telephone'];
 			$_SESSION['helpdeskrole'] = $infos[0]['role'];
 			$_SESSION['helpdeskidRole'] = $infos[0]['idRole'];
-			// $_SESSION['helpdeskidEntreprise'] = $infos[0]['idEntreprise'];
-
+		
 			
 			if($infos[0]['idRole'] == 1){
 				$_SESSION['helpdeskadministrateur'] = true;
 				echo 1;
 			}
-			else if($infos[0]['idRole'] == 4){
-				$_SESSION['helpdeskentreprise'] = true;
-				echo 4;
-			}
 			else if($infos[0]['idRole'] == 2){
-				$_SESSION['helpdeskdirection'] = true;
+				$_SESSION['helpdesktechnicien'] = true;
 				echo 2;
 			}
 			else if($infos[0]['idRole'] == 3){
-				$_SESSION['helpdeskcomptabilite'] = true;
+				$_SESSION['helpdesksimple'] = true;
 				echo 3;
 			}
-			else if($infos[0]['idRole'] == 4){
-				$_SESSION['helpdeskmaitresse'] = true;
-				echo 4;
-			}
-			else if($infos[0]['idRole'] == 5){
-				$_SESSION['helpdeskdirecteurtechnique'] = true;
-				echo 5;
-			}
-			else if($infos[0]['idRole'] == 6){
-				$_SESSION['helpdeskfinance'] = true;
-				echo 6;
-			}
+
 			
 		}else{
 			echo 40;
