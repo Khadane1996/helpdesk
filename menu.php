834525d73b1651467bed6e17180109f5d1bbd63d
SaliouGithub <serignesalioudione@esp.sn>
2022-08-22 13:16:56 +0000

    new

--- menu.php
+++ menu.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="../../../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">HelpDesk</span>
     </a>
 
@@ -21,7 +21,7 @@
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
           <li class="nav-item">
-            <a href="dashboard.php" class="nav-link ">
+            <a href="../../../php/view/dashboard/liste.php" class="nav-link ">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
                 Tableau de bord
@@ -30,7 +30,7 @@
             </a>
           </li>
           <li class="nav-item">
-            <a href="php/view/utilisateur/liste.php" class="nav-link">
+            <a href="../../../php/view/utilisateur/liste.php" class="nav-link">
               <i class="nav-icon fas fa-users"></i>
               <p>
                 Utilisateurs
@@ -39,7 +39,7 @@
             </a>
           </li>
           <li class="nav-item">
-            <a href="php/view/role/liste.php" class="nav-link">
+            <a href="../../../php/view/role/liste.php" class="nav-link">
               <i class="nav-icon fas fa-users-cog"></i>
               <p>
                 Role
@@ -48,7 +48,7 @@
             </a>
           </li>
           <li class="nav-item">
-            <a href="php/view/ticket/liste.php" class="nav-link">
+            <a href="../../../php/view/ticket/liste.php" class="nav-link">
               <i class="nav-icon fas fa-tags"></i>
               <p>
                 Tickets
---
+++ php/view/dashboard/liste.php
@@ -0,0 +1,206 @@
+
+  <!-- Include header et menu début -->
+  <?php
+    include('../../../header.php');
+    include('../../../menu.php');
+  ?>
+  <!-- Include header et menu fin -->
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    
+      <!-- Main Début  -->
+      <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                  <h1 class="m-0">Tableau de bord</h1>
+                </div>
+                
+            </div>
+        </div>
+      </div>
+
+  <!-- Main content -->
+      <section class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-3 col-6">
+                            <!-- small box -->
+                            <div class="small-box bg-info">
+                                <div class="inner">
+                                    <h3>15</h3>
+
+                                    <p>Tickets ouverts</p>
+                                </div>
+                                <div class="icon">
+                                    <i class="ion ion-bag"></i>
+                                </div>
+                                <a href="#" class="small-box-footer">Plus d'info<i class="fas fa-arrow-circle-right"></i></a>
+                            </div>
+                        </div>
+                        <!-- ./col -->
+                        <div class="col-lg-3 col-6">
+                            <!-- small box -->
+                            <div class="small-box bg-success">
+                                <div class="inner">
+                                    <!-- <h3>53<sup style="font-size: 20px">%</sup></h3> -->
+                                    <h3>53</h3>
+
+                                    <p>Tickets fermés</p>
+                                </div>
+                                <div class="icon">
+                                    <i class="ion ion-stats-bars"></i>
+                                </div>
+                                <a href="#" class="small-box-footer">Plus d'info<i class="fas fa-arrow-circle-right"></i></a>
+                            </div>
+                        </div>
+                        <!-- ./col -->
+                        <div class="col-lg-3 col-6">
+                            <!-- small box -->
+                            <div class="small-box bg-warning">
+                                <div class="inner">
+                                    <h3>44</h3>
+
+                                    <p>User Registrations</p>
+                                </div>
+                                <div class="icon">
+                                    <i class="ion ion-person-add"></i>
+                                </div>
+                                <a href="#" class="small-box-footer">Plus d'info<i class="fas fa-arrow-circle-right"></i></a>
+                            </div>
+                        </div>
+                        <!-- ./col -->
+                        <div class="col-lg-3 col-6">
+                            <!-- small box -->
+                            <div class="small-box bg-danger">
+                                <div class="inner">
+                                    <h3>2</h3>
+
+                                    <p>Agent</p>
+                                </div>
+                                <div class="icon">
+                                    <i class="ion ion-person-add"></i>
+                                </div>
+                                <a href="#" class="small-box-footer">Plus d'info<i class="fas fa-arrow-circle-right"></i></a>
+                            </div>
+                        </div>
+                        <!-- ./col -->
+                    </div>
+                </div>
+      </section>
+      <!-- /.content -->
+      <!-- Main Fin -->
+
+  </div>
+
+   <!-- Include footer début -->
+   <?php
+      include('../../../footer.php');
+   ?>
+  <!-- Include footer fin -->
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
