728f7593f0ff4ee9cc363f57b9f2dc038897af96
SaliouGithub <serignesalioudione@esp.sn>
2022-08-31 10:05:12 +0000

    new

--- php/view/dashboard/liste.php
+++ php/view/dashboard/liste.php
@@ -40,108 +40,12 @@
 <body class="hold-transition sidebar-mini layout-fixed">
 
 
-<div class="wrapper">
-
-  <!-- Preloader -->
-  <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="../../../dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
-  </div>
-
-  <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <!-- Left navbar links -->
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-      </li>
-
-    </ul>
-
-    <!-- Right navbar links -->
-    <ul class="navbar-nav ml-auto">
-      <!-- Navbar Search -->
-      <li class="nav-item">
-        <a class="nav-link" data-widget="navbar-search" href="#" role="button">
-          <i class="fas fa-search"></i>
-        </a>
-        <div class="navbar-search-block">
-          <form class="form-inline">
-            <div class="input-group input-group-sm">
-              <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
-              <div class="input-group-append">
-                <button class="btn btn-navbar" type="submit">
-                  <i class="fas fa-search"></i>
-                </button>
-                <button class="btn btn-navbar" type="button" data-widget="navbar-search">
-                  <i class="fas fa-times"></i>
-                </button>
-              </div>
-            </div>
-          </form>
-        </div>
-      </li>
-
-    
-      <li class="nav-item">
-        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-          <i class="fas fa-expand-arrows-alt"></i>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-          <i class="fas fa-th-large"></i>
-        </a>
-      </li>
-                  <!-- Dropdown Menu -->
-                  <li class="nav-item dropdown">
-                    <a class="nav-link" data-toggle="dropdown" href="#">
-                      <!-- <button><i class="far fa-user"> Serigne Saliou Dione</i></button> -->
-                      <button type="button" class="btn btn-outline-primary btn-block btn-sm"><i class="fa fa-user"></i> Serigne Saliou Dione</button>
-                      
-                      <!-- <span class="badge badge-danger navbar-badge">3</span> -->
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-       
-                      <div class="dropdown-divider"></div>
-                      <a href="#" class="dropdown-item">
-                        <!-- Message Start -->
-                        <div class="media">
-                          <img src="dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
-                          <div class="media-body">
-                            <h3 class="dropdown-item-title">
-                              Nora Silvester
-                              <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
-                            </h3>
-                            <p class="text-sm">The subject goes here</p>
-                            <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-                          </div>
-                        </div>
-                        <!-- Message End -->
-                      </a>
-      
-      
-      
-                      <a href="#" class="dropdown-item">
-                        <!-- Message Start -->
-                        <div class="media">
-                          <div class="media-body">
-                            <button type="button" class="btn btn-outline-primary btn-block btn-sm"><i class="fa fa-lock"></i>  Fermer la session</button>       
-                          </div>
-                        </div>
-                        <!-- Message End -->
-                      </a>
-                   
-                    </div>
-                  </li>
-    </ul>
-  </nav>
-  <!-- /.navbar -->
-
-  <!-- Include menu début -->
-  <?php
+ <!-- Include navbar et menu début -->
+ <?php
+    include('../../../navbar.php');
     include('../../../menu.php');
   ?>
-  <!-- Include menu fin -->
+  <!-- Include navbar et menu fin -->
 
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
